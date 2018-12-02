abbdc741d97c6f2f078f38b3c316b2799e117f9c
Shmat00ff <shmat00ff@gmail.com>
2018-12-03 00:05:52 +0700

    Draw commit

--- case6.py
+++ case6.py
@@ -18,43 +18,55 @@ def draw_hex(x):
         right(60)
     right(120)
 
-def rows(x):
-    for i in range(int(x+1/2)):
+def rows1(x,y):
         up()
-        goto(-250, 250 - 500 / (2 * x) - (2 * i * size_hex(x)) - size_hex(x))
+        goto(-250, 250 - 500 / (2 * x) - (3 * y * size_hex(x)) - size_hex(x))
         down()
 
-def draw(x, color1="yellow", color2="green"):
-    for k in range(int(x)):
-        if not k % 2:
-            fillcolor(color1)
-        if k % 2:
-            fillcolor(color2)
-        begin_fill()
-        draw_hex(x)
-        end_fill()
+def rows2(x,y):
     up()
-    goto(-250, 250 - 500 / (2 * x))
+    goto(-250, 250 - 500 / (2 * x) - (3 * y * size_hex(x)) - size_hex(x))
+    back(size_hex(x))
     right(60)
     back(size_hex(x))
     left(60)
-    back(size_hex(x))
     down()
-    for l in range(int(x)):
-        if not l % 2:
-            fillcolor(color1)
-        if l % 2:
-            fillcolor(color2)
-        begin_fill()
-        draw_hex(x)
-        end_fill()
-    rows(x)
 
-
-
-
-
-
+def draw(x, color1="yellow", color2="green"):
+    col1 = color1
+    col2 = color2
+    if not x % 2:
+        v = x
+    if x % 2:
+        v = x + 1
+    for o in range(int(v)//2):
+        rows1(x,o)
+        for k in range(int(x)):
+            if o % 2:
+                color1, color2 = col2, col1
+            if not o % 2:
+                color1, color2 = col1, col2
+            if not k % 2:
+                fillcolor(color1)
+            if k % 2:
+                fillcolor(color2)
+            begin_fill()
+            draw_hex(x)
+            end_fill()
+    for t in range(int(x)//2):
+        rows2(x,t)
+        for k in range(int(x)):
+            if t % 2:
+                color1, color2 = col2, col1
+            if not t % 2:
+                color1, color2 = col1, col2
+            if not k % 2:
+                fillcolor(color1)
+            if k % 2:
+                fillcolor(color2)
+            begin_fill()
+            draw_hex(x)
+            end_fill()
 
 
 def main():
@@ -68,7 +80,6 @@ def main():
     goto(-250, 250 - 500 / (2 * n))
     left(90)
     down()
-    for i in range(n)
     draw(n, c1, c2)
     done()
 
